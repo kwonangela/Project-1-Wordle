7f66b3a9d432ff2fabd945a2caf544e182f947b7
kwonangela <kwonangelaa@gmail.com>
2022-12-01 00:27:05 -0500

    Changed text file to const array & imported/exported to work on browser.  Fixed modal and other css.

--- project1-attempt2.js
+++ project1-attempt2.js
@@ -1,16 +1,13 @@
-// next few lines of code from google
-// reads text file and puts each word into array
-const fs = require("fs");
-const text = fs.readFileSync("./samplewords.txt", "utf-8");
-const wordArr = text.replace(/\s/g, ` `).split(" ");
+// imports the wordList array from words.js, which has all the words to choose from
+import { wordList } from "./words.js";
 
 // Picks the random word from text file
-function pickWord(arr){
-    let randIndex = Math.floor(Math.random() * (arr.length));
-    let randWord = arr[randIndex];
+function pickWord(){
+    let randIndex = Math.floor(Math.random() * (wordList.length));
+    let randWord = wordList[randIndex];
     return randWord;
 }
-let answerWord = pickWord(wordArr);
+let answerWord = pickWord();
 console.log("Answer: " + answerWord);
 
 // Check if user's word matches the answer word
@@ -27,7 +24,7 @@ function checkWord(userWord, answer){
 
 // Check if each letter is in the answer word and in the right spot
 function checkEachLetter(userWord, answer){
-    console.log(`${userWord} + ${answerWord}`);
+    console.log(`${userWord} & ${answerWord}`);
     for (let i=0; i<userWord.length; i++){
         if (answer.includes(userWord[i]) === true){
             console.log(`the word has ${userWord[i]}`);
@@ -45,3 +42,28 @@ function checkEachLetter(userWord, answer){
 
 let inputWord = "hello";
 checkEachLetter(inputWord, answerWord);
+
+// /////////////////////////////
+// // Get the modal
+var modal = document.getElementById("myModal");
+// Get the button that opens the modal
+var btn = document.getElementById("rules");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
